21913a61c99515b26aeabb883541f7ad03aa8eac
Augusto Moreno <augusto.cgm020306@gmail.com>
2026-05-09 13:24:31 -0300

    feat: múltiplos erros corrigidos

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-// O nome no teu pubspec.yaml é 'jogo_forca'
 import 'package:jogo_forca/main.dart';
 
 void main() {
